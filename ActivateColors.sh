1c635bf04af802a0cf66a50b036d9cb6b6152fae
jonathanburgossaldivia <jonathanburgoss@outlook.com>
2017-11-11 16:33:44 -0300

    Add files via upload

--- ActivateColors.sh
+++ ActivateColors.sh
@@ -6,7 +6,11 @@ clear && echo 'Ya se ha modificado la apariencia de Teminal.'
 
 else
 
-clear && echo 'export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\]\$ "
+clear && echo '
+prompt1="\n\e[30;1m[ \e[37m\e[1muser\e[1m\e[34m:\e[0m\e[33;1m\u \e[30;1m] [ \e[37m\e[1mdir\e[1m\e[34m:\e[0m\e[33;1m\w\e[0m\e[30;1m ]\e[0m\e[33;33m\n\\$ \[$(tput sgr0)\]"
+prompt2="\n\e[30;1m\u ( \e[0m\e[33;1m\w\e[0m\e[30;1m )\e[0m\e[33;33m\n\\$ \[$(tput sgr0)\]"
+prompt3="\n\e[30;1m[ \e[37m\e[1muser\e[1m\e[34m:\e[37m\e[1m\u \e[30;1m] [ \e[0m\e[33;1mdir\e[1m\e[34m:\e[0m\e[33;1m\w\e[0m\e[30;1m ]\e[0m\e[33;33m\n\\$ \[$(tput sgr0)\]"
+PS1=$prompt1
 export CLICOLOR=1
 export LSCOLORS=ExFxBxDxCxegedabagacad
 export GREP_OPTIONS="--color=auto"' >> /Users/$USER/.profile
